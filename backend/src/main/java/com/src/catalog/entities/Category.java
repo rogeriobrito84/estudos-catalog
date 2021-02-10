3fff0aaa32e5b849acdaac867e907e094bbcd614
Rogério Oliveira <rogeriobrito84@gmail.com>
2021-02-10 19:34:11 -0300

    h2 database

--- backend/src/main/java/com/src/catalog/entities/Category.java
+++ backend/src/main/java/com/src/catalog/entities/Category.java
@@ -1,12 +1,17 @@
 package com.src.catalog.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
